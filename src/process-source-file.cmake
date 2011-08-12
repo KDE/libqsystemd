1b1e148290fdd3bfc5b1dac613844b2d69efb52b
Stefan Majewsky <majewsky@gmx.net>
2011-08-12 20:20:46 +0200

    Fix build: .h.in/.cpp.in can only be processed after interface.h

--- src/process-source-file.cmake
+++ src/process-source-file.cmake
@@ -18,20 +18,19 @@
 # which declare and define QsdUnit and its subclasses.
 # The signature is: qsystemd_process_source_file(SRCVARIABLE basename1 basename2 ...)
 
-macro(qsystemd_process_source_file _srcvar)
+macro(qsystemd_process_source_file _srcvar _basename _iface)
 	set(PY_FILE ${CMAKE_CURRENT_SOURCE_DIR}/process-source-file.py)
-	foreach(_basename ${ARGN})
-		set(IN_BASE ${CMAKE_CURRENT_SOURCE_DIR}/${_basename})
-		set(OUT_BASE ${CMAKE_CURRENT_BINARY_DIR}/${_basename})
-		set(DEPENDS ${PY_FILE})
-		if(EXISTS ${IN_BASE}.cpp.in)
-			set(DEPENDS ${IN_BASE}.cpp.in ${DEPENDS})
-		endif(EXISTS ${IN_BASE}.cpp.in)
-		add_custom_command(OUTPUT ${OUT_BASE}.h ${OUT_BASE}_p.h ${OUT_BASE}.cpp
-			COMMAND ${PY_FILE} ${IN_BASE} ${OUT_BASE}
-			MAIN_DEPENDENCY ${IN_BASE}.h.in DEPENDS ${DEPENDS}
-			COMMENT "Processing ${_basename}.{h,cpp}.in"
-		)
-		set (${_srcvar} ${${_srcvar}} "${OUT_BASE}.cpp")
-	endforeach(_basename)
+	set(IN_BASE ${CMAKE_CURRENT_SOURCE_DIR}/${_basename})
+	set(OUT_BASE ${CMAKE_CURRENT_BINARY_DIR}/${_basename})
+	set(IFACE_FILE ${CMAKE_CURRENT_BINARY_DIR}/${_iface}.h)
+	set(DEPENDS ${PY_FILE} ${IFACE_FILE})
+	if(EXISTS ${IN_BASE}.cpp.in)
+		set(DEPENDS ${IN_BASE}.cpp.in ${DEPENDS})
+	endif(EXISTS ${IN_BASE}.cpp.in)
+	add_custom_command(OUTPUT ${OUT_BASE}.h ${OUT_BASE}_p.h ${OUT_BASE}.cpp
+		COMMAND ${PY_FILE} ${IN_BASE} ${OUT_BASE}
+		MAIN_DEPENDENCY ${IN_BASE}.h.in DEPENDS ${DEPENDS}
+		COMMENT "Processing ${_basename}.{h,cpp}.in"
+	)
+	set (${_srcvar} ${${_srcvar}} "${OUT_BASE}.cpp")
 endmacro(qsystemd_process_source_file)
